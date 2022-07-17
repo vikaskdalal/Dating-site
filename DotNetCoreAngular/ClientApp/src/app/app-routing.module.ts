54949fad3702fb07fed7c91b649c72b0a63dbea7
Vikas Dalal <vikaskdalal@gmail.com>
2022-07-17 18:03:27 +0530

    created the message component for client and showed the message thread

--- DotNetCoreAngular/ClientApp/src/app/app-routing.module.ts
+++ DotNetCoreAngular/ClientApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserLikeComponent } from './user-like/user-like.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'user/edit', component: EditUserComponent, canActivate : [AuthGuard], canDeactivate : [PreventUnsavedChangesGuard]},
   {path: 'friends', component: UserListComponent, canActivate : [AuthGuard]},
+  {path: 'messages', component: MessageComponent},
   {path: 'user/:username', component: UserDetailComponent, canActivate : [AuthGuard]},
   {path: 'user-likes', component: UserLikeComponent, canActivate : [AuthGuard]},
   {path: 'server-error', component: ServerErrorComponent},
--- DotNetCoreAngular/ClientApp/src/app/message/message.component.ts
+++ DotNetCoreAngular/ClientApp/src/app/message/message.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  messages! : Message[] | null;
+  pagination! : Pagination | null;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
+  constructor(private _messageService : MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages(){
+    this._messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(res => {
+      this.messages = res.result;
+      this.pagination = res.pagination;
+    })
+  }
+
+  pageChanged(event : any){
+    this.pageNumber = event.page;
+    this.loadMessages()
+  }
+
+  loadNewMessages(container : string){
+    this.container = container;
+    this.loadMessages();
   }
 
 }
